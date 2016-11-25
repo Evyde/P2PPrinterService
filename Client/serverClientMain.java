5e3fb58bd354d19c061fb02fa1b806edd1363aaf
ForeverOpp <life.app.hanfeng@hotmail.com>
2016-11-25 22:40:09 +0800

    Add Some Files

---
+++ Client/serverClientMain.java
@@ -0,0 +1,8 @@
+//package P2PPrintService.Client;
+import P2PPrinterService.Common.UDPP2P;
+public class serverClientMain {
+	public static void main(String[] args) {
+		UDPP2P u = new UDPP2P();
+		u.connect();
+	}
+}
---
+++ Common/UDPP2P.java
@@ -0,0 +1,14 @@
+package P2PPrinterService.Common;
+public class UDPP2P {
+	/**
+	*
+	**/
+	public UDPP2P() {
+		;
+	}
+	
+	public int connect() {
+		System.out.println("HelloWorld!");
+		return 0;
+	}
+}
